872804360108bb109afccbcdc85a6c18d2d9200d
jonaw2005 <68852290+jonaw2005@users.noreply.github.com>
2022-12-06 13:06:56 +0100

    Update script.js

--- apps/script.js
+++ apps/script.js
@@ -2,3 +2,12 @@ function update(){
   var div = document.getElementById("div1");
   div.innerHTML = "content2";
 }
+
+
+function canv(){
+  const canvas = document.getElementById("canvas");  
+  const ctx = canvas.getContext("2d");
+  
+  ctx.fillText("TEST", 50, 50);
+  
+}
