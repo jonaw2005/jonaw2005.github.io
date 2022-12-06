4ab722481ca29e4a8aded14bd55cc234b7fc9829
jonaw2005 <68852290+jonaw2005@users.noreply.github.com>
2022-12-06 11:14:47 +0100

    Update script.js

--- apps/script.js
+++ apps/script.js
@@ -1,2 +1,9 @@
 alert("test");
 document.getElementById("div1").innerHTML = "test";
+
+function update(){
+  var div = document.getElementById("div1");
+  div.innerHTML = "test";
+  alert("divtest");
+  
+}
