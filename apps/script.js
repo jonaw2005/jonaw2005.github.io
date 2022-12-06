fb1d010f16ee5cc0f7884cd3a361f79178c2ef60
jonaw2005 <68852290+jonaw2005@users.noreply.github.com>
2022-12-06 13:22:10 +0100

    Update script.js

--- apps/script.js
+++ apps/script.js
@@ -10,4 +10,22 @@ function canv(){
   
   ctx.fillText("TEST", 50, 50);
   
+  let textArr = ["H", "A", "L", "L", "O", "!"];
+  let x = 0;
+  let text = "";
+  
+  for(let i = 0; i<6; i++){
+    text = text + textArr[i];
+    wait(1000);
+    
+  }
+  
+}
+
+function wait(ms) {
+    var start = Date.now(),
+        now = start;
+    while (now - start < ms) {
+      now = Date.now();
+    }
 }
