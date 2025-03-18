d7481562ac7a1306a1393ecf824e057a73c9f877
jonaw2005 <jona@wwwinkler.de>
2025-03-18 15:34:53 +0100

    update

--- scripts/home.js
+++ scripts/home.js
@@ -1,6 +1,7 @@
 // Display a message when the website is loaded or reloaded
 function showUnderConstructionMessage() {
-    alert("This website is under construction and will be updated soon.");
+    //alert("This website is under construction and will be updated soon.");
+    console.log("This website is under construction and will be updated soon.");
 }
 
 // Call the function when the page loads
