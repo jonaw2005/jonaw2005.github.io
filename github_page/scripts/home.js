5651824437e8a870de33b0517a6f442ba4bc8456
jonaw2005 <jona@wwwinkler.de>
2025-03-18 15:04:17 +0100

    added a background and moved the index.html

---
+++ github_page/scripts/home.js
@@ -0,0 +1,7 @@
+// Display a message when the website is loaded or reloaded
+function showUnderConstructionMessage() {
+    alert("This website is under construction and will be updated soon.");
+}
+
+// Call the function when the page loads
+window.onload = showUnderConstructionMessage;
